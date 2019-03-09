a3f0fcdba0e27a9579ff40ca451d78e28e044a35
ADSumADS <45086773+ADSumADS@users.noreply.github.com>
2019-03-08 22:00:05 -0800

    Add files via upload
    more compartmentalized and interface-driven

--- life.js
+++ life.js
@@ -1,26 +1,51 @@
-qKit.init({border:{width:10,color:'#824222',}});
+qKit.init({border:{width:8,color:'#824222'}});
 
-const numCols	=42;
-const numRows	=numCols;
-const cellSize	=qKit.screen.width/numRows;
+const suffusion	=160;
 
 var habitat=new Array(2);
-
-habitat[0]=new Array(numRows);
-habitat[1]=new Array(numRows);
+habitat[0]=new Array(suffusion);
+habitat[1]=new Array(suffusion);
+for(var r=0;r<suffusion;r++)
+{
+	habitat[0][r]=new Array(suffusion);
+	habitat[1][r]=new Array(suffusion);
+}
 
 var phase=0;
+var auto=false;
 
-for(var r=0;r<numRows;r++)
+function Random5050Habitat()
 {
-	habitat[0][r]=new Array(numCols);
-	habitat[1][r]=new Array(numCols);
-
-	for(var c=0;c<numCols;c++)
+	for(var r=0;r<suffusion;r++)for(var c=0;c<suffusion;c++)
 		habitat[phase][r][c]=Math.floor((Math.random()*2));
 }
 
-for(var r=0;r<numRows;r++)for(var c=0;c<numRows;c++)qKit.draw.quad
+function PopulateOneWalker()
+{
+	var m=Math.floor(suffusion/2);
+
+	for(var r=0;r<suffusion;r++)for(var c=0;c<suffusion;c++)habitat[phase][r][c]=0;
+
+	habitat[phase][m-1][m+1]=1;
+	habitat[phase][m  ][m+1]=1;
+	habitat[phase][m+1][m+1]=1;
+	habitat[phase][m+1][m  ]=1;
+	habitat[phase][m  ][m-1]=1;
+}
+
+function ComputeNextPhase()
+{
+	for(var r=0;r<suffusion;r++)for(var c=0;c<suffusion;c++)
+	{
+		var a=0;for(var j=-1;j<=1;j++)for(var i=-1;i<=1;i++)if((i!=0)||(j!=0))
+			a+=habitat[phase][(suffusion+r+j)%suffusion][(suffusion+c+i)%suffusion];
+
+		habitat[(phase+1)%2][r][c]=(a==3 || (a==2 && habitat[phase][r][c]==1))?1:0;
+	}
+}
+
+const cellSize	=qKit.screen.width/suffusion;
+for(var r=0;r<suffusion;r++)for(var c=0;c<suffusion;c++)qKit.draw.quad
 ({
 	x:c*(	cellSize),y:r*(	cellSize),
 	width:	cellSize,height:cellSize,
@@ -33,18 +58,14 @@ for(var r=0;r<numRows;r++)for(var c=0;c<numRows;c++)qKit.draw.quad
 
 const matrix=qKit.groups('matrix');
 
-qKit.update(() => 
+function RenderCurrentPhase()
 {
-	for(var r=0;r<numRows;r++)for(var c=0;c<numCols;c++)
-	{
-		var a=0;for(var j=-1;j<=1;j++)for(var i=-1;i<=1;i++)if((i!=0)||(j!=0))
-			a+=habitat[phase][(numRows+r+j)%numRows][(numCols+c+i)%numCols];
-
-		habitat[(phase+1)%2][r][c]=(a==3 || (a==2 && habitat[phase][r][c]==1))?1:0;
-	}
-
-	phase=(phase+1)%2;
-
 	for(var i=0;i<matrix.length;i++)
-		matrix[i].color=habitat[phase][Math.floor(i/numRows)][i%numCols]?'#C0FFC0':'#000000';
-});
+		matrix[i].color=habitat[phase][Math.floor(i/suffusion)][i%suffusion]?'#D0FFC0':'#102040';
+}
+
+Random5050Habitat();
+//PopulateOneWalker();
+RenderCurrentPhase();
+
+qKit.update(() => {if(auto){ComputeNextPhase();phase=(phase+1)%2;RenderCurrentPhase();}});
